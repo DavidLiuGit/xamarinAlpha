19b1208e77d155de10e6ab8962a2906e49e37e26
DavidYoloLiu <dvdgege5@hotmail.com>
2016-05-10 17:19:11 -0400

    Add event listener and event handler for clicking of the "convert" button

--- XamarinAlpha/MainActivity.cs
+++ XamarinAlpha/MainActivity.cs
@@ -6,22 +6,59 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
-namespace XamarinAlpha
-{
+namespace XamarinAlpha {
     [Activity(Label = "XamarinAlpha", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
-    {
-        int count = 1;
+    public class MainActivity : Activity {
+        //int count = 1;
 
-        protected override void OnCreate(Bundle bundle)
-        {
+        /// <summary>
+        /// The following method is called when the activitiy is initialized, and its contents are created
+        /// </summary>
+        /// <param name="bundle"></param>
+        protected override void OnCreate(Bundle bundle) {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
             // first change
 
-            // deleted button
+            // deleted original content
+            // code will go below:            
+            Button convert = FindViewById<Button>(Resource.Id.buttonConvert);
+            Button call = FindViewById<Button>(Resource.Id.buttonCall);
+
+            // disable call button at the beginning
+            call.Enabled = false;
+
+            // add event listener for clicking of the "Convert!" button
+            convert.Click += Convert_Click;
+
+
+        }
+
+        /// <summary>
+        /// called when the "Convert!" button is clicked. This function will parse the input found in the text field
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Convert_Click(object sender, EventArgs e) {
+            string convertedString = "";                                        // initialize convertedString to an empty string
+
+            // link the call button & text field
+            Button call = FindViewById<Button>(Resource.Id.buttonCall);
+            EditText numText = FindViewById<EditText>(Resource.Id.textNumber);
+
+            convertedString = Core.PhonewordTranslator.ToNumber(numText.Text);  // covnert the string using the method provided
+
+            if (String.IsNullOrWhiteSpace(convertedString)) {                   // if the conversion appears to be INVALID
+                call.Text = "Call!";
+                call.Enabled = false;
+            } else {
+                call.Text = "Call " + convertedString +"!";
+                call.Enabled = true;
+            }
+
+            //throw new NotImplementedException();
         }
     }
 }
