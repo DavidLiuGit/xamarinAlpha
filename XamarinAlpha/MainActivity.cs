abeda5a291739579168520650b60127296a25810
DavidYoloLiu <dvdgege5@hotmail.com>
2016-05-10 23:13:24 -0400

    Add icons.  Add ability to make calls.

--- XamarinAlpha/MainActivity.cs
+++ XamarinAlpha/MainActivity.cs
@@ -8,8 +8,10 @@ using Android.OS;
 using Android.Content.PM;
 
 namespace XamarinAlpha {
-    [Activity(Label = "XamarinAlpha", MainLauncher = true, Icon = "@drawable/icon", ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]
+    [Activity(Label = "Xamarin Alpha", MainLauncher = true, Icon = "@drawable/icon", ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]
     public class MainActivity : Activity {
+        string convertedString = "";                                        // initialize convertedString to an empty string
+
         /// <summary>
         /// The following method is called when the activitiy is initialized, and its contents are created
         /// </summary>
@@ -19,7 +21,6 @@ namespace XamarinAlpha {
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
-            // first change
 
             // deleted original content
             // code will go below:            
@@ -29,19 +30,31 @@ namespace XamarinAlpha {
             // disable call button at the beginning
             call.Enabled = false;
 
-            // add event listener for clicking of the "Convert!" button
+            // add event listener for "Convert!" button
             convert.Click += Convert_Click;
 
+            // add event listener for "Call ...!" button
+            call.Click += Call_Click;
 
+        }
+
+        private void Call_Click(object sender, EventArgs e) {
+            var callDiag = new AlertDialog.Builder(this);
+            callDiag.SetMessage("Call " + convertedString + "?");
+
+            callDiag.SetNeutralButton("Call ", delegate {
+                // create intent to dial phone
+                var callIntent = new Intent(Intent.ActionCall);
+                callIntent.SetData(Android.Net.Uri.Parse("tel:" + convertedString));        // give the intent it's target to dial
+                StartActivity(callIntent);                                      // execute call intent
+            });
         }
 
         /// <summary>
         /// called when the "Convert!" button is clicked. This function will parse the input found in the text field
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void Convert_Click(object sender, EventArgs e) {
-            string convertedString = "";                                        // initialize convertedString to an empty string
+            convertedString = "";
 
             // link the call button & text field
             Button call = FindViewById<Button>(Resource.Id.buttonCall);
@@ -52,7 +65,7 @@ namespace XamarinAlpha {
             if (String.IsNullOrWhiteSpace(convertedString)) {                   // if the conversion appears to be INVALID
                 call.Text = "Call!";
                 call.Enabled = false;
-            } else {                                                            // if the covnersion appears to be valid, then append the converted string to the call button, and allow it to be used
+            } else {                                                            // if the conversion appears to be valid, then append the converted string to the call button, and allow it to be used
                 call.Text = "Call " + convertedString + "!";
                 call.Enabled = true;
             }
